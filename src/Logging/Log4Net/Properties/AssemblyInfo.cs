a5a977a6d9f239abd8c0c48cf87c1dbb8e41af88
Geoffrey <gvd8@hotmail.com>
2015-05-09 09:54:05 +0200

    set assembly version

--- src/Logging/Log4Net/Properties/AssemblyInfo.cs
+++ src/Logging/Log4Net/Properties/AssemblyInfo.cs
@@ -5,13 +5,13 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Log4Net")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("GoSimpleLoggingLog4Net")]
+[assembly: AssemblyDescription("Log4Net implementatation for GoSimpleLogging")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Log4Net")]
+[assembly: AssemblyCompany("Geoffrey Vandiest")]
+[assembly: AssemblyProduct("GoSimpleLogging")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyTrademark("Geoffrey Vandiest")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
